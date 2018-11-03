57397d511e8a11c85b55fdfec2288344a913aa90
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-03 19:18:20 +0600

    Calculated perimeter of a triangle

---
+++ ws2/src/main/java/it/sevenbits/Triangle.java
@@ -0,0 +1,51 @@
+package it.sevenbits;
+
+/**
+ *
+ */
+public class Triangle implements Polygon {
+    private int firstSide;
+    private int secondSide;
+    private int thirdSide;
+
+    /**
+     * constructor Triangle
+     */
+    private Triangle() {
+    }
+
+    /**
+     * @param firstSide  first side
+     * @param secondSide second side
+     * @param thirdSide  third side
+     * @throws TriangleException exception
+     */
+    public Triangle(final int firstSide, final int secondSide, final int thirdSide) throws TriangleException {
+        if (((firstSide + secondSide) <= thirdSide) ||
+                ((firstSide + thirdSide) <= secondSide) || ((thirdSide + secondSide) <= firstSide)) {
+            throw new TriangleException("the triangle does not exist");
+        }
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.thirdSide = thirdSide;
+    }
+
+    /**
+     * @return sb.toString
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("first side = ").append(firstSide).append("\n").append("second side = ").append(secondSide).append("\n").append("third side = ").append(thirdSide);
+        return sb.toString();
+    }
+
+    /**
+     * @return perimeter
+     */
+    @Override
+    public int perimeter() {
+        return firstSide + secondSide + thirdSide;
+    }
+}
+
