4c889dd3490f4a8491e79b5b733f73aa8c41f892
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-30 21:48:45 +0600

    Exceptions for triangle

---
+++ src/main/java/it/sevenbits/exceptions/TriangleException.java
@@ -0,0 +1,13 @@
+package it.sevenbits.exceptions;
+
+/**
+ * exception situation
+ */
+public final class TriangleException extends Exception {
+    /**
+     * @param message exception
+     */
+    public TriangleException(final String message) {
+        super(message);
+    }
+}
